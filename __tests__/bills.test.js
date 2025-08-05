825b7312545d52ed257862fd57c70b65c73f8420
elizabethvilloria <lizvilloria@gmail.com>
2025-08-05 17:22:15 +0200

    test: add tests for bills api

---
+++ __tests__/bills.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const server = require('../server/server');
+
+describe('Bills API', () => {
+    it('should fetch all bills', async () => {
+        const res = await request(server).get('/api/bills');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.message).toBe('success');
+    });
+
+    it('should create a new bill', async () => {
+        const res = await request(server)
+            .post('/api/bills')
+            .send({
+                name: 'Test Bill',
+                amount: 100,
+                due_date: '2024-12-31'
+            });
+        expect(res.statusCode).toEqual(201);
+        expect(res.body.data.name).toBe('Test Bill');
+    });
+
+    it('should update a bill', async () => {
+        const res = await request(server)
+            .patch('/api/bills/1')
+            .send({
+                is_paid: true
+            });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.data.is_paid).toBe(true);
+    });
+}); 
